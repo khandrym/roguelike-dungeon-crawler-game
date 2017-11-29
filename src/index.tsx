8fe03664a427316b4e0717a140b1f5f659a85caf
Andrii Khandymailov <khandrym@gmail.com>
2017-11-29 12:56:14 +0200

    Move App.tsx to containers directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import App from './containers/App';
 import './index.css';
 
 ReactDOM.render(
