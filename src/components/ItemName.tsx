5b047c8bb7bdfab6f6b086243100c441ed2dd797
Andrii Khandymailov <khandrym@gmail.com>
2018-01-02 13:08:23 +0200

    Delete old unnecessary files

--- src/components/ItemName.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '../theme/styled-components';
-
-export default styled.div`
-  font-size: ${props => props.theme.fontSizeItemName};
-  border-bottom-width: 1px;
-  border-bottom-style: solid;
-  margin-bottom: 10px;
-`;
--- src/components/Quality.tsx
+++
@@ -1,6 +0,0 @@
-import styled from '../theme/styled-components';
-
-export default styled.div`
-  padding-left: 10px;
-  font-size: 14px;
-`;
