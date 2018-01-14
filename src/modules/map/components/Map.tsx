24a0f1da1305d8b06e6aacc5634e87a68d7aa7b6
Andrii Khandymailov <khandrym@gmail.com>
2018-01-14 08:23:33 +0200

    Replace usage of styled-components by ordinary css for map module

--- src/modules/map/components/Map.tsx
+++ src/modules/map/components/Map.tsx
@@ -1,57 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { styled, Theme } from '../../theme';
-import ItemTypes from '../../itemTypes';
 import * as store from '../../store';
-
-const Table = styled.table`
-  border-collapse: collapse;
-`;
-
-interface PropsTableData {
-  itemType: ItemTypes;
-  className?: string;
-  theme?: Theme;
-}
-
-const TableData: React.StatelessComponent<PropsTableData> = (props) => {
-  return (
-    <td className={props.className}>
-      {props.children}
-    </td>
-  );
-};
-
-const StyledTableData = styled(TableData) `
-  width: 5px;
-  height: 5px;
-  border-width: 1px;
-  border-style: solid;
-  border-color: ${props => props.theme.colorBorder};
-  border-collapse: collapse;
-  background-color: ${(props) => {
-    switch (props.itemType) {
-      case ItemTypes.PLAYER:
-        return props.theme.colorPrimary;
-      case ItemTypes.DARKNESS:
-        return props.theme.colorDarkness;
-      case ItemTypes.GROUND:
-        return props.theme.colorGround;
-      case ItemTypes.WALL:
-        return props.theme.colorWall;
-      case ItemTypes.ENEMY:
-        return props.theme.colorEnemy;
-      case ItemTypes.WEAPON:
-        return props.theme.colorWeapon;
-      case ItemTypes.MEDICINE:
-        return props.theme.colorMedicine;
-      case ItemTypes.GATE:
-        return props.theme.colorGate;
-      default:
-        return props.theme.colorPrimary;
-    }
-  }};
-`;
+import './Map.css';
 
 interface MapStateProps {
   playerX: number;
@@ -69,9 +19,9 @@ function Map(props: MapProps) {
     var cells = [];
     for (let i = 0; i < 100; i++) {
       if (i === props.playerX && j === props.playerY) {
-        cells[i] = <StyledTableData itemType={ItemTypes.PLAYER} />;
+        cells[i] = <td className="MapCell MapCell-player" />;
       } else {
-        cells[i] = <StyledTableData itemType={ItemTypes.GROUND} />;
+        cells[i] = <td className="MapCell MapCell-ground" />;
       }
     }
     rows[j] = (
@@ -81,9 +31,11 @@ function Map(props: MapProps) {
     );
   }
   return (
-    <Table>
-      {rows}
-    </Table>
+    <table className="Map">
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
   );
 }
 
