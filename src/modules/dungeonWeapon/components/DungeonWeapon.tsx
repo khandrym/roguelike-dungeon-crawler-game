ef616ba4ae60763950aab06223e1b90072d24371
Andrii Khandymailov <khandrym@gmail.com>
2018-01-22 13:16:24 +0200

    Add DungeonWeapon component

---
+++ src/modules/dungeonWeapon/components/DungeonWeapon.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Wrapper from './Wrapper';
+import * as item from '../../item';
+
+interface Props {
+  readonly name: string;
+  readonly attack: number;
+  readonly x: number;
+  readonly y: number;
+  readonly dungeonName: string;
+}
+
+export default (props: Props) => {
+  return (
+    <Wrapper>
+      <item.components.Name name={props.name} />
+      <item.components.Property>
+        Attack: {props.attack}
+      </item.components.Property>
+      <item.components.Location
+        x={props.x}
+        y={props.y}
+        dungeonName={props.dungeonName}
+      />
+    </Wrapper>
+  );
+};
