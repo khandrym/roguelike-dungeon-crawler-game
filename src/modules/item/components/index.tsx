2ccac0abe6d31dfee0dd2848f6cc1ba1b22b9f26
Andrii Khandymailov <khandrym@gmail.com>
2018-01-25 13:45:48 +0200

    Refactor exports of item module

---
+++ src/modules/item/components/index.tsx
@@ -0,0 +1,19 @@
+import Wrapper from './Wrapper';
+import Experience from './Experience';
+import Health from './Health';
+import Level from './Level';
+import Locatin from './Location';
+import Name from './Name';
+import Property from './Property';
+import Weapon from './Weapon';
+
+export default {
+  Wrapper: Wrapper,
+  Experience: Experience,
+  Health: Health,
+  Level: Level,
+  Location: Locatin,
+  Name: Name,
+  Property: Property,
+  Weapon: Weapon
+};
