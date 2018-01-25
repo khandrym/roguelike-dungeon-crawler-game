0f32f618d6e47d2913cd182c377c6199fd30ddf3
Andrii Khandymailov <khandrym@gmail.com>
2018-01-25 13:07:06 +0200

    Refactor exports of dungeonEnemy module

--- src/modules/dungeonEnemy/index.tsx
+++ src/modules/dungeonEnemy/index.tsx
@@ -1,16 +1,5 @@
-import * as m from './model';
-import * as a from './actions';
-import at from './actionTypes';
-import r from './reducer';
-import DungeonEnemy from './components/DungeonEnemy';
-
-export const model = m;
-export const actions = a;
-export const ActionTypes = at;
-export const reducer = r;
-
-export interface State extends m.State { }
-
-export const components = {
-  DungeonEnemy: DungeonEnemy
-};
+export * from './model';
+export * from './actions';
+export { default as ActionTypes } from './actionTypes';
+export { default as reducer } from './reducer';
+export { default as components } from './components';
