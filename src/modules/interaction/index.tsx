6a433ed659cf41f2bb00e26a1623fdfce0412866
Andrii Khandymailov <khandrym@gmail.com>
2018-04-20 11:30:26 +0300

    Modify interaction module

--- src/modules/interaction/index.tsx
+++ src/modules/interaction/index.tsx
@@ -1,24 +1,49 @@
 import * as store from '../../modules/store';
 import * as locations from '../locations';
+import * as neighborItem from '../neighborItem';
+import ItemTypes from '../itemTypes';
+// import * as enemies from '../enemies';
 
 const onKeyPress = (event: KeyboardEvent) => {
   const state = store.getState();
   const player = store.getPlayer(state);
+  let neighbor;
+  let playerMovement;
   switch (event.key) {
     case 'ArrowRight':
-      store.dispatch(locations.moveRight(player.locationId));
+      neighbor = neighborItem.getNeighborItemAtRight(state);
+      playerMovement = locations.moveRight;
       break;
     case 'ArrowLeft':
-      store.dispatch(locations.moveLeft(player.locationId));
+      neighbor = neighborItem.getNeighborItemAtLeft(state);
+      playerMovement = locations.moveLeft;
       break;
     case 'ArrowUp':
-      store.dispatch(locations.moveUp(player.locationId));
+      neighbor = neighborItem.getNeighborItemAtTop(state);
+      playerMovement = locations.moveUp;
       break;
     case 'ArrowDown':
-      store.dispatch(locations.moveDown(player.locationId));
+      neighbor = neighborItem.getNeighborItemAtBottom(state);
+      playerMovement = locations.moveDown;
       break;
     default:
       break;
+  }
+  if (neighbor && playerMovement) {
+    switch (neighbor.itemType) {
+      case ItemTypes.ENEMY:
+        // const enemy = enemies.getOne(state, neighbor.itemId);
+        break;
+      case ItemTypes.GATE:
+        break;
+      case ItemTypes.MEDICINE:
+        break;
+      case ItemTypes.WEAPON:
+        break;
+      default:
+        store.dispatch(playerMovement(player.locationId));
+        break;
+    }
   }
 };
 
