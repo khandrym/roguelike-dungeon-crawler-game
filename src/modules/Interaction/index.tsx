1494a3f184c38a10db700a93702bb345d3a533a3
Andrii Khandymailov <khandrym@gmail.com>
2018-04-23 16:01:23 +0300

    Fix calculation of damage to player and damage to enemy

--- src/modules/Interaction/index.tsx
+++ src/modules/Interaction/index.tsx
@@ -11,32 +11,32 @@ import * as Player from '../Player';
 const onKeyPress = (event: KeyboardEvent) => {
   const state = Store.getState();
   const player = Store.getPlayer(state);
-  let neighbor;
+  let neighborItem;
   let playerMovement;
   switch (event.key) {
     case 'ArrowRight':
-      neighbor = NeighborItem.getNeighborItemAtRight(state);
+      neighborItem = NeighborItem.getNeighborItemAtRight(state);
       playerMovement = Locations.moveRight;
       break;
     case 'ArrowLeft':
-      neighbor = NeighborItem.getNeighborItemAtLeft(state);
+      neighborItem = NeighborItem.getNeighborItemAtLeft(state);
       playerMovement = Locations.moveLeft;
       break;
     case 'ArrowUp':
-      neighbor = NeighborItem.getNeighborItemAtTop(state);
+      neighborItem = NeighborItem.getNeighborItemAtTop(state);
       playerMovement = Locations.moveUp;
       break;
     case 'ArrowDown':
-      neighbor = NeighborItem.getNeighborItemAtBottom(state);
+      neighborItem = NeighborItem.getNeighborItemAtBottom(state);
       playerMovement = Locations.moveDown;
       break;
     default:
       break;
   }
-  if (neighbor && playerMovement) {
-    switch (neighbor.itemType) {
+  if (neighborItem && playerMovement) {
+    switch (neighborItem.itemType) {
       case ItemTypes.DUNGEON_ENEMY:
-        const dungeonEnemy = DungeonEnemies.getOne(state, neighbor.itemId);
+        const dungeonEnemy = DungeonEnemies.getOne(state, neighborItem.itemId);
         if (dungeonEnemy.health <= 0) {
           Store.dispatch(playerMovement(player.locationId));
         } else {
@@ -44,8 +44,12 @@ const onKeyPress = (event: KeyboardEvent) => {
           const playerAttack = Weapons.getOne(state, player.weaponId).attack;
           const enemyExperience = Enemies.getOne(state, dungeonEnemy.enemyId).experience;
           const enemyAttack = Weapons.getOne(state, Enemies.getOne(state, dungeonEnemy.enemyId).weaponId).attack;
-          const damageToPlayer = (enemyAttack + enemyExperience * (0.5)) / (playerExperience / 10);
-          const damageToEnemy = (playerAttack + playerExperience * (0.5)) / (enemyExperience / 10);
+          const damageToPlayer = Math.floor(
+            (enemyAttack + enemyExperience * 0.5 * (1 + Math.random())) /
+            (playerExperience / 10));
+          const damageToEnemy = Math.floor(
+            (playerAttack + playerExperience * 0.5 * (1 + Math.random())) /
+            (enemyExperience / 10));
           Store.dispatch(Player.decreaseHealth(damageToPlayer));
           Store.dispatch(DungeonEnemies.decreaseHealth(dungeonEnemy.id, damageToEnemy));
         }
@@ -53,7 +57,7 @@ const onKeyPress = (event: KeyboardEvent) => {
       case ItemTypes.DUNGEON_GATE:
         break;
       case ItemTypes.DUNGEON_MEDICINE:
-        const dungeonMedicine = DungeonMedicines.getOne(state, neighbor.itemId);
+        const dungeonMedicine = DungeonMedicines.getOne(state, neighborItem.itemId);
         if (dungeonMedicine.health <= 0) {
           Store.dispatch(playerMovement(player.locationId));
         }
